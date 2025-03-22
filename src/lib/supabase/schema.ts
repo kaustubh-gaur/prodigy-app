4caff3e8d22600a43b9d42018cc7c55c821144e8
kaustubh-gaur <kaustubhgaur.work@gmail.com>
2025-03-23 02:45:18 +0530

    deal with some bugs

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -8,10 +8,45 @@ export const workspaces = pgTable("workspaces", {
     mode: "string",
   }).default(sql`now()`),
   workspaceOwner: uuid("workspace_owner").notNull(),
-  title: text("title").notNull(), // Fixed: added parentheses
+  title: text("title").notNull(),
   iconId: text("icon_id").notNull(),
   data: text("data"),
   inTrash: text("in_trash"),
   logo: text("logo"),
   bannerUrl: text("banner_url"),
+});
+
+export const folders = pgTable("folders", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  title: text("title").notNull(),
+  iconId: text("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  bannerUrl: text("banner_url"),
+  workspaceId: uuid("workspace_id").references(() => workspaces.id, {
+    onDelete: "cascade",
+  }),
+});
+
+export const files = pgTable("files", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  }).default(sql`now()`),
+  title: text("title").notNull(),
+  iconId: text("icon_id").notNull(),
+  data: text("data"),
+  inTrash: text("in_trash"),
+  bannerUrl: text("banner_url"),
+  workspaceId: uuid("workspace_id").references(() => workspaces.id, {
+    onDelete: "cascade",
+  }),
+  folderId: uuid("folder_id").references(() => folders.id, {
+    onDelete: "cascade",
+  }),
 });
